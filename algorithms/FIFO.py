e0e679a03848d42dd08109b57c0e422566bc0cc6
kjano10 <kuba.jano3@gmail.com>
2021-03-21 13:19:38 +0100

    Implement size aware FIFO version

--- algorithms/FIFO.py
+++ algorithms/FIFO.py
@@ -1,12 +1,39 @@
 class FIFO():
 
-  def __init__(self):
+  def __init__(self, memory_size, files_size):
+    self.memory_size = memory_size
+    self.size = 0
     self.queue = []
-    
-  def initialize(self, memory):
-    self.queue = memory
+    self.swap_count = 0
+    self.files_size = files_size
 
-  def swap(self, file):
+
+  def process(self, file):
+    file_size = self.files_size[file]
+
+    if file in self.queue:
+      return
+
+    if self.size + file_size <= self.memory_size:
+      self.size += file_size
+      self.queue.append(file)
+    else:
+      self.swap(file, file_size)
+
+    print(self.queue)
+    print(self.size)
+
+
+  def swap(self, file, file_size):
+    while self.size + file_size > self.memory_size:
+      self.size -= self.files_size[self.queue[0]]
+      self.queue.pop(0)
+      self.swap_count += 1
+
+    self.size += file_size
     self.queue.append(file)
-    self.queue.pop(0)
     return self.queue
+
+
+  def get_swap_count(self):
+    return self.swap_count
--- main.py
+++ main.py
@@ -5,37 +5,27 @@ from algorithms.FIFO import FIFO
 class SwapAlgorithmEvalutor():
 
   def __init__(self):
-    self.load_data()
-    self.count = 0
     self.memory = []
     self.swap_count = 0
-    self.algorithm = FIFO()
-    self.initialized = False
+    self.algorithm = None
 
   def load_data(self):
     with open('data.json') as json_file:
       data = json.load(json_file)
-      self.memory_count = data['memory_count']
+      self.memory_size = data['memory_size']
       self.file_count = data['file_count']
+      self.files_size = data['files_size']
       self.order = data['order']
+
+    self.algorithm = FIFO(self.memory_size, self.files_size)
 
   def process_workflow(self):
     for file in self.order:
-      if self.count < self.memory_count:
-        self.memory.append(file)
-        self.memory = list(set(self.memory))
-        self.count += 1
-      else:
-        if not self.initialized:
-          self.algorithm.initialize(self.memory)
-          self.initialized = True
+      self.algorithm.process(file)
 
-        if not file in self.memory:
-          self.memory = self.algorithm.swap(file)
-          self.swap_count +=1
-
-    print("Swap count: ", self.swap_count)
+    print("Swap count: ", self.algorithm.get_swap_count())
 
 if __name__ == '__main__':
   SAEvaluator = SwapAlgorithmEvalutor()
+  SAEvaluator.load_data()
   SAEvaluator.process_workflow()
