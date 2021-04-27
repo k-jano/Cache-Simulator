e09297ca4970e29f3cc39ea793ae83ac6937fa9a
kjano10 <kuba.jano3@gmail.com>
2021-04-27 08:44:41 +0200

    Add redis dependency, check tasks key contents

--- hyperflow_evaluator.py
+++ hyperflow_evaluator.py
@@ -1,4 +1,5 @@
 import json
+import redis
 
 from flask import Flask, request
 app = Flask(__name__)
@@ -9,5 +10,19 @@ def process():
     print('Process job %s' % body)
     return 'Processed'
 
+
+
 if __name__ == "__main__":
-    app.run()
+    wf_key = 1
+    keys = []
+
+    r = redis.StrictRedis(host="localhost", port=6379, db=0)
+    for key in r.scan_iter("wf:" + str(wf_key) + ":task:[0-9]*:*"):
+        print(key)
+        keys.append(key)
+
+    
+
+    # for key in keys:
+    #     r.zrange(key, 0, -1)
+    
